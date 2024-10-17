ac58625ff7e18caeafd1bb302b24dbcab3ee9793
pragyan <ghimirepragyan100@gmail.com>
2024-10-17 21:57:15 +0545

    optimized  code

--- index.js
+++ index.js
@@ -18,74 +18,67 @@ app.use(express.static(path.join(__dirname, "public"))); // Assuming 'public' is
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const api_key = API_KEY;
-let lat = 27.708317;
-let lon = 85.3205817;
+//default Biratnagar
+let lat = 26.4525;
+let lon = 87.2718;
 let units = "metric";
 
 app.get("/", async (req, res) => {
   try {
-    //current weather data
-    const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=${units}`
-    );
-    const data = response.data;
-    // console.log(response);
-    //forecast weather data
-    const nresponse = await axios.get(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}&units=${units}`
-    );
-    const ndata = nresponse.data;
-
-    res.render("index.ejs", {
-        data: data,
-        ndata: ndata,
-        unit: units,
-      });
+    getWeatherData(res, lon, lat, units);
   } catch (error) {
     console.error(error.message);
   }
 });
 app.post("/", async (req, res) => {
   try {
-    const location = req.body.searchLocation;
-    // console.log(location);
-    const resGeoApi = await axios.get(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${api_key}`
-    );
-    const result = resGeoApi.data;
-    lat = result[0].lat;
-    lon = result[0].lon;
-    // console.log(lat + " " + lon);
-
-    //current weather data
-    const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=${units}`
-    );
-    const data = response.data;
-
-    //forecast weather data
-    const nresponse = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}&units=${units}`
-      );
-      const ndata = nresponse.data;
-  
-      res.render("index.ejs", {
-          data: data,
-          ndata: ndata,
-          unit: units,
-        });
+    directGeocoding(req,res);
+    // getWeatherData(res, lon, lat, units);
   } catch (error) {
     console.error(error.message);
   }
 });
-app.get("/metric",(req,res)=>{
-    units = "metric";
-    res.redirect("/");
+app.get("/metric", async (req, res) => {
+  units = "metric";
+  res.redirect("/");
 });
-app.get("/imperial",(req,res)=>{
-    units = "imperial";
-    res.redirect("/");
+app.get("/imperial", async (req, res) => {
+  units = "imperial";
+  res.redirect("/");
 });
 app.listen(port, () => {
   console.log(`Sever is running in port: ${port}`);
 });
+
+async function getWeatherData(res, lon, lat, units) {
+  //current weather data
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=${units}`
+  );
+  const data = response.data;
+  console.log(data.name);
+  //forecast weather data
+  const nresponse = await axios.get(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}&units=${units}`
+  );
+  const ndata = nresponse.data;
+
+  res.render("index.ejs", {
+    data: data,
+    ndata: ndata,
+    unit: units,
+  });
+}
+
+async function directGeocoding(req,res) {
+  const location = req.body.searchLocation;
+  // console.log(location);
+  const resGeoApi = await axios.get(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${api_key}`
+  );
+  const result = resGeoApi.data;
+  lat = result[0].lat;
+  lon = result[0].lon;
+  // console.log(`${lat}, ${lon}`);
+  getWeatherData(res, lon, lat, units);
+}
